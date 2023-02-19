3b5147702eaa451db2b3bc9f9ea3f6e9297610b1
IconAkpe <michael.akpe@yahoo.com>
2023-02-19 13:22:07 -0800

    printf in bytes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,3 +1,8 @@
+/*
+ * File: 6-size.c
+ * Auth: IconAkpe
+ */
+
 #include <stdio.h>
 
 /**
@@ -8,11 +13,11 @@
  */
 int main(void)
 {
-	printf("Size of chaer: %zu byte(s)\n", sizeof(char));
+	printf("Size of char: %zu byte(s)\n", sizeof(char));
 	printf("Size of an int: %zu byte(s)\n", sizeof(int));
 	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
-	printf("Size ofa float: %zu byte(s)\n", sizeof(float));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 
 	return (0);
 }
