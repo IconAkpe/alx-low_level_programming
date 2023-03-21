b3321633ea2fda6deb74e7ec1a0f8646e29c22ed
IconAkpe <michael.akpe@yahoo.com>
2023-03-20 18:31:26 -0700

    prints the name of program compiled

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -6,19 +6,6 @@
  *
  * Return: Always 0.
  */
-int main(void)
-{
-	printf("%s\n", __FILE__);
-
-	return (0);
-#include <stdio.h>
-
-/**
- * main - Prints the name of the file of the program
- *        was compiled from, followed by a new line.
- *
- * Return: Always 0.
- */
 
 	int main(void)
 {
